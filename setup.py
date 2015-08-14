d2da1f9fb85e4a6680b84518472c20bc717f905a
Martin Olveyra <molveyra@gmail.com>
2015-08-14 10:19:53 -0300

    forgot to rename here

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'frontera_scrapy',
+    name         = 'scrapy_frontera',
     version      = '0.1',
     packages     = find_packages(),
 )
