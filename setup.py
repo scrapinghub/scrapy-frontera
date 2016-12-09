8b7726345a8895418a3011ddcb6ab9ba51ffd458
Martin Olveyra <molveyra@gmail.com>
2016-12-09 16:02:01 -0300

    added version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'scrapy_frontera',
-    version      = '0.1',
+    version      = '0.1.0',
     packages     = find_packages(),
 )
