db0a0717f5091d052ff7f7208b94e13c025b0566
Martin Olveyra <molveyra@gmail.com>
2017-06-08 14:43:21 -0300

    bump version 0.1.2->0.1.3

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'scrapy_frontera',
-    version      = '0.1.2',
+    version      = '0.1.3',
     packages     = find_packages(),
 )
