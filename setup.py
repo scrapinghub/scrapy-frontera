c4e87726efb7bd3f2f48f7e8266e1f5f235000fd
Martin Olveyra <molveyra@gmail.com>
2018-04-16 12:31:09 -0300

    update version to 0.1.8

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'scrapy_frontera',
-    version      = '0.1.7',
+    version      = '0.1.8',
     packages     = find_packages(),
 )
