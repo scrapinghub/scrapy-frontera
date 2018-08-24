0a5f4416c360afd4f381719a6748ee3c1caa3ca1
Martin Olveyra <molveyra@gmail.com>
2018-08-24 21:22:44 +0000

    updated to version 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'scrapy_frontera',
-    version      = '0.2',
+    version      = '0.2.1',
     packages     = find_packages(),
     install_requires=('frontera==0.7.1',)
 )
