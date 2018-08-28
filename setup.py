1b5aa74f08435cdf793d0457068bcd53558acb84
Martin Olveyra <molveyra@gmail.com>
2018-08-28 09:20:05 -0300

    added LICENSE file and opensource info in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'scrapy_frontera',
+    name         = 'scrapy-frontera',
+    description  = 'Featured Frontera scheduler for Scrapy',
     version      = '0.2.1',
+    licence      = 'BSD',
+    url          = 'https://github.com/scrapinghub/hcf-backend',
+    author_email = 'info@scrapinghub.com',
+    maintainer   = 'Scrapinghub',
+    maintainer_email = 'info@scrapinghub.com',
     packages     = find_packages(),
-    install_requires=('frontera==0.7.1',)
+    install_requires=('frontera==0.7.1',),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
 )
